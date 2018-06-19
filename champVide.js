b3d2a157550356d3ea5579f6f423d48a1a2ae092
NicolasSIO <31706940+NicolasSIO@users.noreply.github.com>
2018-06-19 17:38:30 +0200

    Add files via upload

--- champVide.js
+++ champVide.js
@@ -1,83 +1,67 @@
-function champEstVide(){
-	var champ1 = document.getElementById('nouveauMdp').value;
-	var champ2 = document.getElementById('confirmation').value;
+function champEstVideId(){
+	var champId = document.getElementById('identifiant');
+	var erreurId = document.getElementById('erreur_identifiant');
+	var erreurOk = document.getElementById('envoi_ok');
+	var erreurNok = document.getElementById('envoi_erreur');
 	var hasError = false;
 	
-	if(champ1 == ""){
-		document.getElementById('erreur-nouveauMdp').innerHTML = 'Le champ nouveau mot de passe est vide';
-		document.getElementById('confirmation').value = '';
-		document.getElementById('envoi_erreur').innerHTML = "Votre mot de passe n'a pas été modifié"
-		document.getElementById('envoi_erreur').style.display = "block";
+	if(champId.value == ""){
+		erreurId.innerHTML = 'Le champ identifiant est vide';
 		hasError = true;
-	}
-	if(champ2 == ""){
-		document.getElementById('erreur-confirmation').innerHTML = 'Le champ confirmation est vide';
-		document.getElementById('nouveauMdp').value = '';
-		document.getElementById('envoi_erreur').innerHTML = "Votre mot de passe n'a pas été modifié"
-		document.getElementById('envoi_erreur').style.display = "block";
-		hasError = true;
-	}
-	if(champ1 != ""&& champ2 != "" && champ1 != champ2){
-		document.getElementById('erreur-confirmation').innerHTML = 'Les champs nouveau de mot de passe et confirmation sont différents';
-		document.getElementById('nouveauMdp').value = '';
-		document.getElementById('confirmation').value = '';
-		document.getElementById('envoi_erreur').innerHTML = "Votre mot de passe n'a pas été modifié"
-		document.getElementById('envoi_erreur').style.display = "block";
-		hasError = true;
-	}
-	if(champ1 != "" && champ2 != "" && champ1 == champ2){
-		document.getElementById('envoi_ok').innerHTML = "Votre mot de passe a bien été modifié"
-		document.getElementById('envoi_ok').style.display = "block";
-		document.getElementById('envoi_erreur').style.display = "none";
-		document.getElementById('nouveauMdp').value = '';
-		document.getElementById('confirmation').value = '';
-	}else{
-		document.getElementById('envoi_ok').style.display = "none";
 	}
 	
 	if (hasError) {
+		erreurNok.innerHTML = "Il y a eu une erreur lors de la saisie de votre identifiant";
+		erreurNok.style.display = "block";
+		erreurOk.style.display = "none";
+		champId.value = '';
 		return true;
 	} else {
+		erreurOk.innerHTML = "Vous allez recevoir un mail avec un lien vous permettant de modifier votre mot de passe";
+		erreurOk.style.display = "block";
+		erreurNok.style.display = "none";
 		return false;
 	}
 }
 
-function champEstVide2(){
-	var champ1 = document.getElementById('identifiant').value;
+function champEstVideMdp(){
+	var champNouvMdp = document.getElementById('nouveauMdp');
+	var champConfirm = document.getElementById('confirmation');
+	var erreurNouvMdp = document.getElementById('erreur_nouveauMdp');
+	var erreurConfirm = document.getElementById('erreur_confirmation');
+	var erreurOk = document.getElementById('envoi_ok');
+	var erreurNok = document.getElementById('envoi_erreur');
 	var hasError = false;
 	
-	if(champ1 == ""){
-		document.getElementById('identifiant').value = '';
-		document.getElementById('envoi_erreur').innerHTML = "Il y a eu une erreur lors de la saisie de votre identifiant"
-		document.getElementById('envoi_erreur').style.display = "block";
-		document.getElementById('envoi_ok').style.display = "none";
+	if(champNouvMdp.value == ""){
+		erreurNouvMdp.innerHTML = 'Le champ nouveau mot de passe est vide';
 		hasError = true;
-	}else{
-		document.getElementById('envoi_ok').innerHTML = "Vous allez recevoir un mail dans votre boîte mail"
-		document.getElementById('envoi_ok').style.display = "block";
-		document.getElementById('envoi_erreur').style.display = "none";
-		document.getElementById('identifiant').value = '';
-		hasError = false;
+	}
+	if(champConfirm.value == ""){
+		erreurConfirm.innerHTML = 'Le champ confirmation est vide';
+		hasError = true;
+	}
+	if(champNouvMdp.value != ""&& champConfirm.value != "" && champNouvMdp.value != champConfirm.value){
+		erreurConfirm.innerHTML = 'Les champs nouveau de mot de passe et confirmation sont différents';
+		hasError = true;
 	}
 	
 	if (hasError) {
+		erreurNok.innerHTML = "Votre mot de passe n'a pas été modifié";
+		erreurNok.style.display = "block";
+		erreurOk.style.display = "none";		
+		champNouvMdp.value = '';
+		champConfirm.value = '';
 		return true;
 	} else {
+		erreurOk.innerHTML = "Votre mot de passe a bien été modifié";
+		erreurOk.style.display = "block";
+		erreurNok.style.display = "none";
 		return false;
 	}
 }
 
-function effacerErreur(){
-	var champ1 = document.getElementById('nouveauMdp').value;
-	var champ2 = document.getElementById('confirmation').value;
-	
-	if(champ1 != ""){
-		document.getElementById('erreur-nouveauMdp').innerHTML = "";
-	}
-	if(champ2 != ""){
-		document.getElementById('erreur-confirmation').innerHTML = "";
-	}
-	if(champ1 != "" && champ2 != "" && champ1 == champ2){
-		document.getElementById('erreur-confirmation').innerHTML = "";
-	}
+function effacerErreurGenerique(champ){
+	var idDivErreur = 'erreur_' + champ.id;
+	document.getElementById(idDivErreur).innerHTML = "";
 }
